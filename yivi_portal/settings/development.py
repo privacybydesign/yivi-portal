6dd6989bd5587b28c137e20602e69a52aaf5d73d
Jasper <10500389+jappe999@users.noreply.github.com>
2025-01-31 14:29:15 +0000

    chore: Local development setup (#7)
    * chore: Repurpose compose file for development
    
    * docs: Update instructions to set up the project using compose
    
    * docs: docker-compose is now meant to use during development
    
    ---------
    
    Co-authored-by: Jasper van der Linden <jappe999@users.noreply.github.com>

--- yivi_portal/settings/development.py
+++ yivi_portal/settings/development.py
@@ -9,7 +9,7 @@ SECRET_KEY = "django-insecure-!ar%l1@3bkc52!3l(a^$x9j46p#7wlxm^2$l$rtp8d1m-=dvf)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(",")
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
