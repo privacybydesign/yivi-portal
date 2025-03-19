5c5b1e9745a5d9dfe87f6d0bb48dbbee6d700301
Dibran Mulder <dibran.mulder@outlook.com>
2025-03-19 12:00:47 +0100

    Everything is up and running

--- portal_frontend/next.config.ts
+++ portal_frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
+  output: "standalone",
   images: {
     unoptimized: true,
     remotePatterns: [
--- portal_frontend/src/pages/_app.tsx
+++ portal_frontend/src/pages/_app.tsx
@@ -26,57 +26,57 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     // Add any additional logout logic here
   };
   return <div className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-  <header className="bg-white shadow-md">
-          <div className="container mx-auto flex justify-between items-center p-4">
-            <Image src="/yivi-logo.svg" alt="Yivi Logo" height={30} width={54} />
+    <header className="bg-white shadow-md">
+      <div className="container mx-auto flex justify-between items-center p-4">
+        <Image src="/yivi-logo.svg" alt="Yivi Logo" height={30} width={54} />
 
-            <div className="flex-1 pl-4">
-              <Link href="/" className="text-xl">
-                Portal
-              </Link>
-            </div>
+        <div className="flex-1 pl-4">
+          <Link href="/" className="text-xl">
+            Portal
+          </Link>
+        </div>
 
-            <nav className="hidden md:flex gap-6">
-              <Link href="/organizations" className="hover:text-blue-600">
-                Organizations
-              </Link>
-              {email ? (
-                  <div className="flex items-center gap-4">
-                  <span>{email}</span>
-                  <button onClick={handleLogout} className="hover:text-blue-600">
-                    Logout
-                  </button>
-                </div>
-              ) : <Link href="/login" className="hover:text-blue-600">
-                Login
-              </Link>
-              }
-            </nav>
-
-            <div className="md:hidden">
-              <button onClick={() => setIsOpen(!isOpen)} className="p-2">
-                {isOpen ? "Close" : "Menu"}
+        <nav className="hidden md:flex gap-6">
+          <Link href="/organizations" className="hover:text-blue-600">
+            Organizations
+          </Link>
+          {email ? (
+            <div className="flex items-center gap-4">
+              <span>{email}</span>
+              <button onClick={handleLogout} className="hover:text-blue-600">
+                Logout
               </button>
             </div>
-          </div>
+          ) : <Link href="/login" className="hover:text-blue-600">
+            Login
+          </Link>
+          }
+        </nav>
 
-          {isOpen && (
-            <div className="md:hidden bg-white shadow-md absolute w-full left-0 top-0 h-screen flex flex-col items-center justify-center z-50">
-              <Link href="/" className="block py-2 hover:text-blue-600">
-                Home
-              </Link>
-              <Link href="/about" className="block py-2 hover:text-blue-600">
-                About
-              </Link>
-              <Link href="/services" className="block py-2 hover:text-blue-600">
-                Services
-              </Link>
-              <Link href="/extra" className="block py-2 hover:text-blue-600">
-                Extra
-              </Link>
-            </div>
-          )}
-        </header>
-        <Component {...pageProps} />;
+        <div className="md:hidden">
+          <button onClick={() => setIsOpen(!isOpen)} className="p-2">
+            {isOpen ? "Close" : "Menu"}
+          </button>
         </div>
+      </div>
+
+      {isOpen && (
+        <div className="md:hidden bg-white shadow-md absolute w-full left-0 top-0 h-screen flex flex-col items-center justify-center z-50">
+          <Link href="/" className="block py-2 hover:text-blue-600">
+            Home
+          </Link>
+          <Link href="/about" className="block py-2 hover:text-blue-600">
+            About
+          </Link>
+          <Link href="/services" className="block py-2 hover:text-blue-600">
+            Services
+          </Link>
+          <Link href="/extra" className="block py-2 hover:text-blue-600">
+            Extra
+          </Link>
+        </div>
+      )}
+    </header>
+    <Component {...pageProps} />;
+  </div>
 }
