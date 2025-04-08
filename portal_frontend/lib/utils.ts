85927130e130611b07d7f2d5f07a942544e82402
saravahdatipour <saravahdatipour@gmail.com>
2025-04-08 13:42:40 +0200

    removing some comments

--- portal_frontend/lib/utils.ts
+++ portal_frontend/lib/utils.ts
@@ -2,10 +2,9 @@ export function cn(...classes: (string | boolean | undefined)[]) {
   return classes.filter(Boolean).join(" ");
 }
 
-
 export const generateSlug = (text: string) => {
   return text
     .toLowerCase()
-    .replace(/\s+/g, "-")         // replace spaces with hyphen
-    .replace(/[^a-z0-9-]/g, "");   // allow only lowercase letters, digits, and hyphen
+    .replace(/\s+/g, "-")
+    .replace(/[^a-z0-9-]/g, "");
 };
