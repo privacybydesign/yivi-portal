c00f62df8e9222d226ad1c00b1bc4da933803b6e
Jasper van der Linden <jappe999@users.noreply.github.com>
2025-04-29 11:53:55 +0200

    fix: This should be an id

--- yivi_auth/models/user.py
+++ yivi_auth/models/user.py
@@ -1,6 +1,6 @@
 class User:
-    def __init__(self, email: str, role: str, organizationSlug: str):
+    def __init__(self, email: str, role: str, organizationId: str):
         self.id = email
         self.email = email
         self.role = role
-        self.organizationSlug = organizationSlug
+        self.organizationId = organizationId
