f5a20de5a284e62ca7addf2d162149da9328e440
saravahdatipour <saravahdatipour@gmail.com>
2025-05-08 16:27:15 +0200

    chore: add react version of organizations page and add path in children routes

--- portal_spa/src/routes/index.tsx
+++ portal_spa/src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { useRoutes } from "react-router-dom";
 import HomePage from "@/pages/HomePage";
+import OrganizationsListPage from "@/pages/OrganizationsListPage";
 import Layout from "@/components/layout/Layout";
 
 // We manage all routes here to keep App.tsx clean
@@ -8,7 +9,10 @@ export default function AppRoutes() {
     {
       path: "/",
       element: <Layout />,
-      children: [{ path: "/", element: <HomePage /> }],
+      children: [
+        { path: "/", element: <HomePage /> },
+        { path: "/organizations", element: <OrganizationsListPage /> },
+      ],
     },
   ]);
 }
