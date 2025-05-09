94ccc9a863fe51969e5854cf8bbe5de4f479befc
saravahdatipour <saravahdatipour@gmail.com>
2025-05-09 14:40:22 +0200

    fix: add toaster

--- portal_spa/src/App.tsx
+++ portal_spa/src/App.tsx
@@ -1,9 +1,11 @@
 import AppRoutes from "./routes";
+import { Toaster } from "@/components/ui/toaster";
 
 function App() {
   return (
     <div>
       <AppRoutes />
+      <Toaster />
     </div>
   );
 }
