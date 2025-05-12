e94b374c76aa229ab47f378dcf36748e120033a2
saravahdatipour <saravahdatipour@gmail.com>
2025-05-12 14:27:50 +0200

    refactor: remove nextjs project and github actions workflow

--- portal_frontend/src/models/paginated-response.ts
+++
@@ -1,6 +0,0 @@
-export interface PaginationResponse<T> {
-    count: number;
-    next: string | null;
-    previous: string | null;
-    results: T[];
-}
--- portal_frontend/src/types/yivi-frontend.d.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-declare module '@privacybydesign/yivi-frontend' {
-    const yivi: any;
-    export default yivi;
-}
