e5f167a7aa5c152d3ac5773e676a0c7971f3ee3b
saravahdatipour <saravahdatipour@gmail.com>
2025-05-20 16:27:49 +0200

    test with 2 threads

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@ export DJANGO_SETTINGS_MODULE=yivi_portal.settings.production
 
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-uwsgi --http :8000 --wsgi-file /app/yivi_portal/wsgi.py --master --processes 1 --threads 4 --uid nobody --gid nogroup --disable-logging --static-map ${STATIC_URL}=${STATIC_ROOT} --static-map ${MEDIA_URL}=${MEDIA_ROOT}
+uwsgi --http :8000 --wsgi-file /app/yivi_portal/wsgi.py --master --processes 1 --threads 2 --uid nobody --gid nogroup --disable-logging --static-map ${STATIC_URL}=${STATIC_ROOT} --static-map ${MEDIA_URL}=${MEDIA_ROOT}
