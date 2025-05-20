5957b7ba591559a26906e6648e3e46a411bd847a
Jasper <10500389+jappe999@users.noreply.github.com>
2025-05-20 12:10:06 +0200

    feat: Terms of service (#102)
    Co-authored-by: Jasper van der Linden <jappe999@users.noreply.github.com>

--- portal_spa/src/components/layout/Layout.tsx
+++ portal_spa/src/components/layout/Layout.tsx
@@ -1,5 +1,7 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import Header from "@/components/layout/Header";
+import { buttonVariants } from "../ui/button";
+import { cn } from "@/lib/utils";
 
 export default function Layout() {
   return (
@@ -8,8 +10,24 @@ export default function Layout() {
       <main className="flex-grow p-4">
         <Outlet />
       </main>
-      <footer className="bg-gray-100 text-center p-4">
-        &copy; 2025 Yivi Portal. All rights reserved.
+      <footer className="bg-gray-100 p-4">
+        <div className="flex justify-between items-center container px-4 mx-auto">
+          <p>&copy; 2025 Yivi Portal. All rights reserved.</p>
+          <div className="flex gap-6">
+            <Link
+              to="/terms-of-service"
+              className={cn(buttonVariants({ variant: "link" }))}
+            >
+              Terms of service
+            </Link>
+            <Link
+              to="/privacy-policy"
+              className={cn(buttonVariants({ variant: "link" }))}
+            >
+              Privacy policy
+            </Link>
+          </div>
+        </div>
       </footer>
     </div>
   );
