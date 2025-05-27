2b6d8230f53ef11fe3ba29764b95be74fccdf3df
saravahdatipour <saravahdatipour@gmail.com>
2025-05-27 10:23:25 +0200

    chore: updated credential.ts

--- portal_spa/src/models/credential.ts
+++ portal_spa/src/models/credential.ts
@@ -9,6 +9,11 @@ export interface CredentialAttribute {
 export interface Credential {
   id: number;
   name_en: string;
+  name_nl: string;
+  ap_slug: string;
+  environment: string;
+  org_name: string;
+  org_slug: string;
   credential_id: string;
   description: string;
   attributes: CredentialAttribute[];
