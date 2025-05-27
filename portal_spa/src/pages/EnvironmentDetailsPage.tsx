429fd05576eb323d35b27e2d0873b8d80fdd4ac5
saravahdatipour <saravahdatipour@gmail.com>
2025-05-27 10:21:41 +0200

    chore: add placeholder pages

---
+++ portal_spa/src/pages/EnvironmentDetailsPage.tsx
@@ -0,0 +1,11 @@
+export default function EnvironmentDetailsPage() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold mb-4">Environment Page</h1>
+      <p className="text-lg">
+        This page will display information about the selected environment.
+      </p>
+    </div>
+  );
+}
+// TODO: Implement this
