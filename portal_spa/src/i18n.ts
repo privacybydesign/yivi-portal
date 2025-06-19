1a10722c5ee763acc2497977bc0e1b9664d54392
saravahdatipour <saravahdatipour@gmail.com>
2025-06-19 22:25:48 +0200

    WIP: adding translations for list and details page

--- portal_spa/src/i18n.ts
+++ portal_spa/src/i18n.ts
@@ -1,21 +1,21 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import en from './locales/en/translation.json';
-import nl from './locales/nl/translation.json';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import en from "./locales/en/translation.json";
+import nl from "./locales/nl/translation.json";
 
 const resources = {
   en: { translation: en },
-  nl: { translation: nl }
+  nl: { translation: nl },
 };
 
 const storedLanguage =
-  typeof window !== 'undefined' && localStorage.getItem('language');
+  typeof window !== "undefined" && localStorage.getItem("language");
 
 i18n.use(initReactI18next).init({
   resources,
-  lng: storedLanguage || 'en',
-  fallbackLng: 'en',
-  interpolation: { escapeValue: false }
+  lng: storedLanguage || "en",
+  fallbackLng: "en",
+  interpolation: { escapeValue: false },
 });
 
 export default i18n;
