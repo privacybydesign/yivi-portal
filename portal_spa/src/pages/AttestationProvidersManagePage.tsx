697a77bba34b89a3c1a5411aa0ad58036dee6df9
saravahdatipour <saravahdatipour@gmail.com>
2025-06-24 16:58:27 +0200

    Revert "Add indication that the attestation provider manage page is under development (#186)"
    This reverts commit 0f4d50a66234a41802764257e9749fcd7d918088.

--- portal_spa/src/pages/AttestationProvidersManagePage.tsx
+++ portal_spa/src/pages/AttestationProvidersManagePage.tsx
@@ -1,13 +1,30 @@
+import { fetchOrganization } from "@/actions/manage-organization";
 import ManageOrganizationLayout from "@/components/layout/organization/manage-organization";
+import { Separator } from "@/components/ui/separator";
+import type { Organization } from "@/models/organization";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function AttestationProvider() {
+  const slug = useParams()?.organization;
+  const [organization, setOrganization] = useState<Organization>();
+
+  useEffect(() => {
+    if (slug) {
+      fetchOrganization(slug as string).then((response) =>
+        setOrganization(response?.data)
+      );
+    }
+  }, [slug]);
+
   return (
     <ManageOrganizationLayout>
-      <div className="flex flex-col items-center justify-center h-80 bg-muted rounded-2xl shadow-inner">
-        <h2 className="text-2xl font-semibold mb-2">Under Development</h2>
-        <p className="text-muted-foreground text-center max-w-md">
-          {"This section is coming soon. Stay tuned for updates!"}
-        </p>
+      <div className="space-y-6">
+        <div>
+          <h2 className="text-lg font-medium">Attestation providers (TODO)</h2>
+        </div>
+        <Separator />
+        <div>{organization?.name_en}</div>
       </div>
     </ManageOrganizationLayout>
   );
